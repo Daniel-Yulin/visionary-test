f13fd295927ecd71e9c218d01a138d5c1223e42b
Daniel-Yulin <85147001+Daniel-Yulin@users.noreply.github.com>
2026-03-10 10:16:56 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const message = document.getElementById('message');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    
-    // 讓按鈕顯示處理中
     submitBtn.innerText = 'Connecting...';
     submitBtn.disabled = true;
 
@@ -17,14 +15,11 @@ form.addEventListener('submit', e => {
         body: new URLSearchParams(new FormData(form)) 
     })
     .then(() => {
-        // 隱藏輸入框和按鈕
         form.classList.add('hidden');
-        
-        // 顯示高質感的成功訊息內容
         message.classList.remove('hidden');
         message.innerHTML = `
             <span class="success-title">成功訂閱</span>
-            本期高清原圖與週報將於近期透過 Email 交付。<br>
+            本期原圖與週報將透過 Email 交付。<br>
             請留意來自「視想家」的信件。<br>
             <span class="stay-visionary">Stay Visionary.</span>
         `;
